5b6a2fcd84dcbd42260cfca3c1b90320e96f8f6d
Moonfih <victorhugorodriguesguimaraes@gmail.com>
2023-03-22 20:02:41 -0300

    search broked

--- filmes-teste/src/pages/Home.jsx
+++ filmes-teste/src/pages/Home.jsx
@@ -8,6 +8,7 @@ const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
     const [topMovies, setTopMovies] = useState([])
+    
     const getTopRatedMovies = async (url) => {
         const res = await fetch(url);
         const data = await res.json();
